824e1ffc0a68e6b488887c2fd2e1cb09b71392d2
techraiders <hello_navneet@hotmail.com>
2018-05-16 20:31:42 +0530

    Sends JSON data from express server.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,7 +3,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Hello Express!');
+  //response.send('<h1>Hello Express!</h1>');
+  response.send({
+    name: 'Navneet',
+    likes: [
+      'Biking',
+      'Cities'
+    ]
+  });
 });
 
 app.listen(3000);
