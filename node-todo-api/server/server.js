d3d95a147dcd0d5ff200755dc16d15fe640340e4
techraiders <hello_navneet@hotmail.com>
2018-05-27 19:21:03 +0530

    Installs mongoose, creates mongoose server, todo model using mongoose, and saves a new todo to mongoDB.

---
+++ node-todo-api/server/server.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+// save new something
+const Todo = mongoose.model('todo', {
+  text: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt: {
+    type: Number
+  }
+});
+
+const newTodo = new Todo({
+  text: 'Cook dinner'
+});
+
+newTodo.save().then( (doc) => {
+  console.log('Saved todo ', doc);
+}, (err) => {
+  console.log('Unable to save todo.');
+});
