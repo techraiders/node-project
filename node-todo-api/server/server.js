2c746991215c62e0091e19ee3d60db13c2a14dfb
techraiders <hello_navneet@hotmail.com>
2018-05-27 22:13:21 +0530

    Installs body-parser package, and creates first REST API that responds to HTTP post request and saves request body into mongoDB.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,24 @@
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+const express = require('express'),
+      bodyParser = require('body-parser'),{mongoose} = require('./db/mongoose'),
+      {Todo} = require('./models/todo'),
+      {User} = require('./models/user'),
+      app = express();
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  
+  let toDo = new Todo({
+    text: req.body.text
+  });
+
+  toDo.save().then( (doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
