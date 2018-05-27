0cb3d7a951b5f72274fdb3a61c6ed6213d3d3c98
techraiders <hello_navneet@hotmail.com>
2018-05-27 17:16:44 +0530

    Queries mongoDB database.

---
+++ node-todo-api/playground/mongodb-find.js
@@ -0,0 +1,22 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+  var db = client.db('TodoApp');
+
+  db.collection('Todos').find({
+    _id: new ObjectID('5b0a9476003197a8a7cf0560')
+  }).toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    if (err) console.log('Unable to fetch Todos', err);
+  });
+
+  //db.close();
+});
