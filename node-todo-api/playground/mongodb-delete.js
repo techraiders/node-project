71bdf93a93afb4f1466382602959ce148dbfd90b
techraiders <hello_navneet@hotmail.com>
2018-05-27 18:05:10 +0530

    Adds 2 ways of deleting one record from a collection.

--- node-todo-api/playground/mongodb-delete.js
+++ node-todo-api/playground/mongodb-delete.js
@@ -9,16 +9,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 
   var db = client.db('TodoApp');
 
-  // deleteMany
+  /* deleteMany
   db.collection('Todos').deleteMany({
     text: 'Eat lunch'
   }).then( (result) => {
     console.log(result);
-  });
+  }); */
 
-  // deleteOne
+  /* deleteOne
+  db.collection('Todos').deleteOne({
+    text: 'Eat lunch'
+  }).then( (result) => {
+    console.log(result);
+  }); */
 
   // findOneAndDelete
+  db.collection('Todos').findOneAndDelete({
+    completed: false
+  }).then( (result) => {
+    console.log(result);
+  });
 
   //db.close();
 });
