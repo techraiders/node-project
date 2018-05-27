3ada0764d1cb29e587e2f22e243c200605c81543
techraiders <hello_navneet@hotmail.com>
2018-05-27 17:26:53 +0530

    Returns number of records found using count cursor.

--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -9,11 +9,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 
   var db = client.db('TodoApp');
 
-  db.collection('Todos').find({
-    _id: new ObjectID('5b0a9476003197a8a7cf0560')
-  }).toArray().then((docs) => {
-    console.log('Todos');
-    console.log(JSON.stringify(docs, undefined, 2));
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5b0a9476003197a8a7cf0560')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   if (err) console.log('Unable to fetch Todos', err);
+  // });
+
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
   }, (err) => {
     if (err) console.log('Unable to fetch Todos', err);
   });
