07255f27da209218e4a58f6872bd9cb526547390
techraiders <hello_navneet@hotmail.com>
2018-05-27 23:59:37 +0530

    Handles Object Id not found case while quering.

--- node-todo-api/playground/mongoose-queries.js
+++ node-todo-api/playground/mongoose-queries.js
@@ -1,7 +1,7 @@
 const { mongoose } = require("./../server/db/mongoose");
 const {Todo} = require('./../server/models/todo');
 
-let id = '5b0ab4fea81b660778f4d71b';
+let id = '6b0ab4fea81b660778f4d71b';
 
 Todo.find({
   _id: id
@@ -12,9 +12,11 @@ Todo.find({
 Todo.findOne({
   _id: id
 }).then((todo) => {
+  if (!todo) return console.log('Id not found.');
   console.log('Todo ', todo);
 });
 
 Todo.findById(id).then((todo) => {
+  if (!todo) return console.log('Id not found.');
   console.log('Todo By Id ', todo);
 });
